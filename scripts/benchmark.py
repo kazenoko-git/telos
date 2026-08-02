fd5a8a86a3b4c7cf746f0d78b730cc0772f7c308
Ivan Samuel <kazenoko.main@gmail.com>
2026-08-02 12:05:38 +0530

    fix: remove internal os.environ setting in benchmark.py to prevent double XLA init

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -17,7 +17,6 @@ import os
 
 def detect_device():
     try:
-        os.environ["PJRT_DEVICE"] = "TPU"
         # pyrefly: ignore
         import torch_xla.core.xla_model as xm
         device = xm.xla_device()
