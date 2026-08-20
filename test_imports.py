14bb8e5d4a6a60d823d242705f36d3e9b7cb7019
Ivan Samuel <kazenoko.main@gmail.com>
2026-08-20 16:27:47 +0530

    remove: unnecessary python notebook updation files

--- test_imports.py
+++
@@ -1,5 +0,0 @@
-from telos.training.core import execute_mlx_training_step, clip_grad_norm_mlx
-from ar.training.trainer import TelosMLXARTrainer
-from mdiff.training.trainer import TelosMLXTrainer
-from undiff.training.trainer import TelosMLXUNDLMTrainer
-print("All imports succeeded!")
