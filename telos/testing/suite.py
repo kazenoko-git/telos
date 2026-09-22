b82c0242a93a85b046baafe7e539900b7eab050d
Ivan Samuel <kazenoko.main@gmail.com>
2026-09-22 20:04:59 +0530

    fix: gate mlx.nn import on working Metal device in testing suite

--- telos/testing/suite.py
+++ telos/testing/suite.py
@@ -14,8 +14,10 @@ import numpy as np
 # MLX
 try:
     import mlx.core as mx
-    import mlx.nn as mx_nn
-    MLX_AVAILABLE = True
+    from telos.training.core import metal_usable
+    MLX_AVAILABLE = metal_usable()
+    if MLX_AVAILABLE:
+        import mlx.nn as mx_nn
 except ImportError:
     MLX_AVAILABLE = False
 
